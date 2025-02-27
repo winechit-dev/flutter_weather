2f5bbecc7fa10fc9e345e3c2d63b230be83c4c9f
winechit-dev <winechitpaing.dev@gmail.com>
2025-02-27 12:42:37 +0700

    - chore: create open_meteo_api packages

---
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -0,0 +1,13 @@
+class Location {
+  final int id;
+  final String name;
+  final double latitude;
+  final double longitude;
+
+  const Location({
+      required this.id,
+      required this.name,
+      required this.latitude,
+      required this.longitude
+  });
+}
---
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -0,0 +1,9 @@
+class Weather {
+  final double temperature;
+  final double weathercode;
+
+  Weather({
+    required this.temperature,
+    required this.weathercode
+  });
+}
