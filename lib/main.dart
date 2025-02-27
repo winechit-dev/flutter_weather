ac47d849b4ce3b31465846b6f8d9f42c3e013d5f
winechit-dev <winechitpaing.dev@gmail.com>
2025-02-27 15:06:26 +0700

    - chore: setup entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_weather/app.dart';
+import 'package:flutter_weather/weather_bloc_observer.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = WeatherBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+  runApp(WeatherApp(weatherRepository: WeatherRepository()));
 }
 
 class MyApp extends StatelessWidget {
